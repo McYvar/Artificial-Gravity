c1329e7a420fe5064c6c999faa7894c607c88341
McYvar <yvarftoorop@hotmail.com>
2022-07-28 00:23:31 +0200

    3D camercontroller basis

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     #region Variables
-
+    [SerializeField] Vector3 _3DPosition;
+    [SerializeField] Vector3 angle;
+    [SerializeField] Transform cameraCenter;
+    [SerializeField] Transform player;
 
     #endregion
+
+
+    void Start()
+    {
+        transform.position = _3DPosition;
+        transform.rotation = Quaternion.Euler(angle);
+    }
 
 
     void Update()
@@ -19,7 +29,11 @@ public class CameraController : MonoBehaviour
                 break;
 
             case PerspectiveType._3D:
-
+                cameraCenter.position = player.position;
+                float xRot = Input.GetAxisRaw("Mouse Y");
+                float yRot = Input.GetAxisRaw("Mouse X");
+                cameraCenter.Rotate(Vector3.right * xRot);
+                cameraCenter.Rotate(Vector3.up * yRot);
                 break;
         }
     }
